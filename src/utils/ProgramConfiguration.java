0ba1e2cbf10b5ba9eeb9110bebbec40958c215ac
Martin  Stoyanov <martotbb.2556@gmail.com>
2026-05-07 11:32:03 +0300

    added getters and setters to program configuration class

--- src/utils/ProgramConfiguration.java
+++ src/utils/ProgramConfiguration.java
@@ -12,18 +12,96 @@ public class ProgramConfiguration {
     private static final int    DEFAULT_MAX_ITER = 1000;
     private static final String DEFAULT_OUTPUT   = "mandelbrot.png";
 
-    public int    width    = DEFAULT_WIDTH;
-    public int    height   = DEFAULT_HEIGHT;
-    public double reMin    = DEFAULT_RE_MIN;
-    public double reMax    = DEFAULT_RE_MAX;
-    public double imMin    = DEFAULT_IM_MIN;
-    public double imMax    = DEFAULT_IM_MAX;
-    public int    threads  = DEFAULT_THREADS;
-    public int    maxIter  = DEFAULT_MAX_ITER;
-    public String output   = DEFAULT_OUTPUT;
-    public boolean quiet   = false;
-    public boolean dynamic = false;
+    private int    width    = DEFAULT_WIDTH;
+    private int    height   = DEFAULT_HEIGHT;
+    private double reMin    = DEFAULT_RE_MIN;
+    private double reMax    = DEFAULT_RE_MAX;
+    private double imMin    = DEFAULT_IM_MIN;
+    private double imMax    = DEFAULT_IM_MAX;
+    private int    threads  = DEFAULT_THREADS;
+    private int    maxIter  = DEFAULT_MAX_ITER;
+    private String output   = DEFAULT_OUTPUT;
+    private boolean quiet   = false;
+    private boolean dynamic = false;
 
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setReMax(double reMax) {
+        this.reMax = reMax;
+    }
+
+    public void setReMin(double reMin) {
+        this.reMin = reMin;
+    }
+
+    public double getImMin() {
+        return imMin;
+    }
+
+    public void setImMin(double imMin) {
+        this.imMin = imMin;
+    }
+
+    public double getImMax() {
+        return imMax;
+    }
+
+    public void setImMax(double imMax) {
+        this.imMax = imMax;
+    }
+
+    public int getThreads() {
+        return threads;
+    }
+
+    public void setThreads(int threads) {
+        this.threads = threads;
+    }
+
+    public int getMaxIter() {
+        return maxIter;
+    }
+
+    public void setMaxIter(int maxIter) {
+        this.maxIter = maxIter;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    public void setOutput(String output) {
+        this.output = output;
+    }
+
+    public boolean isQuiet() {
+        return quiet;
+    }
+
+    public void setQuiet(boolean quiet) {
+        this.quiet = quiet;
+    }
+
+    public boolean isDynamic() {
+        return dynamic;
+    }
+
+    public void setDynamic(boolean dynamic) {
+        this.dynamic = dynamic;
+    }
     @Override
     public String toString() {
         return String.format(
