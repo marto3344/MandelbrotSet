eae25f6fb1a005d74e416921e97fcf2e4422c92c
Martin  Stoyanov <martotbb.2556@gmail.com>
2026-05-07 14:54:37 +0300

    improved exception handling in cli parsing

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,18 @@ import utils.CliConfigParser;
 public class Main {
     public static void main(String[] args) {
         ConfigParser parser = new CliConfigParser();
-        ProgramConfiguration cfg = parser.parse(args);
+        ProgramConfiguration cfg = null;
+        try {
+            cfg = parser.parse(args);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+        catch (Exception e) {
+            System.err.println("Fatal error!");
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
         System.out.println(cfg.toString());
     }
 }
